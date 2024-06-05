e009cfed222e282cafec9eb8d167fdaedb7adc22
shinjiyun <ranji12@naver.com>
2024-06-05 11:55:42 +0900

    REST API 인증키 작성

---
+++ index.js
@@ -0,0 +1,21 @@
+const a=`
+https://apis.data.go.kr/6300000/eventDataService/eventDataListJson
+?
+serviceKey
+=
+%2BB%2F1oMdT4N%2FCcpBL1U35jMZOSGuWV8Po6gQvanL9rb%2BI%2BrLJs21Yxi2zxMDy0JrOg47Am%2BeVwy8SAiFAjpQYZw%3D%3D&`
+
+const b = "endpoint 주소" +"주소값";
+
+const keyName = "%2BB%2F1oMdT4N%2FCcpBL1U35jMZOSGuWV8Po6gQvanL9rb%2BI%2BrLJs21Yxi2zxMDy0JrOg47Am%2BeVwy8SAiFAjpQYZw%3D%3D";
+const endPonit = "http://apis.data.go.kr/6300000/eventDataService";
+const example = `
+http://apis.data.go.kr/6300000/eventDataService/eventDataListJson
+?
+serviceKey=${keyName}
+&
+numOfRows=10
+&
+pageNo=1`;
+
+console.log(example)
